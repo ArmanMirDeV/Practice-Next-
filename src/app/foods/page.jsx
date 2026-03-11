49aaaa890ab0c4bbdb81ed87cef5414fe4b7b50f
Arman Mir <mirarman8583@gmail.com>
2026-03-11 11:04:56 +0600

    data fetching

--- src/app/foods/page.jsx
+++ src/app/foods/page.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React from 'react'
 
-const FoodsPage = () => {
-  return <div>FoodsPage</div>;
-};
+const getFoods = async () => {
+  const res = await fetch(
+    " https://taxi-kitchen-api.vercel.app/api/v1/foods/random",
 
-export default FoodsPage;
+  );
+  const data = await res.json()
+  return data.foods || []
+}
+
+const FoodsPage = async () => {
+  
+  const foods = await getFoods()
+
+  return (
+    <div>
+      <h2 className="text-4xl font-bold">
+        {" "}
+        Total <span className='text-yellow-500'>{foods.length}</span> data found
+      </h2>
+    </div>
+  );
+}
+
+export default FoodsPage
